31aaf1aad95652f8523ba2d5ba370f5d8911cb7d
Aliona <aliona030989@mail.ru>
2022-12-22 00:41:32 +0300

    Step 5

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,41 +1,22 @@
 import readlineSync from 'readline-sync';
+
 import startGame from '../index.js';
 
-const contRounds = 3;
-const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const isEven = (num) => {
-if (num %2 === 0) {return 'yes';}
-else {return 'no'};
-}
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const startGame = () => {
-  console.log("Welcome to the Brain Games!");
-    let userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
-    console.log(description);
 
-    const question = getRandom(1, 100);
-    const corrAnswer = isEven(question);
-    return [question, corrAnswer];
-  };
+const isEvenNumber = (num) => num % 2 === 0;
 
-const startGame = () => {
-
-for (let i = 1; i <= contRounds; i += 1) {
-    const [question, corrAnswer] = gamedata();
-    console.log('Question:', question);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === corrAnswer.toString()) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corrAnswer}'.`);
-        console.log(`Let's try again, ${userName}!`);
-        return false;
-      }
-    }
-    console.log(`Congratulations, ${userName}!`);
-    return true;
+const gamedata = () => {
+  const getRandom = (max = 100) => Math.round(Math.random() * max);
+  const question = getRandom();
+  const answer = isEvenNumber(question) ? 'yes' : 'no';
+  return [question, answer];
 };
-export default startGame;
+
+const initGameEven = () => {
+  startGame(description, gamedata);
+};
+
+export default initGameEven;
 
 ///startGame (description, gamedata);
--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import readlineSync from 'readline-sync';
 const contRounds = 3;
 
 const startGame = (description, gamedata) => {
-   console.log("Welcome to the Brain Games!");
-   let userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(description);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 
-for (let i = 1; i <= contRounds; i += 1) {
+  console.log(description);
+  let corrAnswer = 0;
+
+  while (corrAnswer < contRounds) {
     const [question, corrAnswer] = gamedata();
-    console.log('Question:', question);
+    console.log(`'Question:' ${question}`);
     const answer = readlineSync.question('Your answer: ');
+
     if (answer === corrAnswer.toString()) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corrAnswer}'.`);
-        console.log(`Let's try again, ${userName}!`);
-        return false;
-      }
+      console.log('Correct!');
+      
+    } else {
+       console.log (`'${answer}' is wrong answer ;(. Correct answer was '${corrAnswer}'.`);
+       console.log(`Let's try again, ${userName}!`);
+      return false;
     }
-    console.log(`Congratulations, ${userName}!`);
-    return true;
+  }
+  console.log(`Congratulations, ${userName}!`);
+  return true;
 };
 export default startGame;
